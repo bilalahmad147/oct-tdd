a9d7ef889b478702a96938b2969483f333f55191
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2020-10-23 03:07:55 +0500

    testing hello world

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,15 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders hello', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/hello/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('should render hello world', () => {
+  const getText = render(<App />);
+  const headingElement = getText.getByText(/hello world/i);
+  expect(headingElement).toBeInTheDocument();
+})
+
